c0217efbd86167d83e04c6c39a36f2c913092cc6
zunaidhosse <138894191+zunaidhosse@users.noreply.github.com>
2025-06-14 00:36:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,48 +17,26 @@ const urlsToCache = [
   'https://cdn-icons-png.flaticon.com/512/9377/9377574.png'
 ];
 
-self.addEventListener('install', event => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        // Add all URLs to cache, but don't fail the entire install if one external URL fails
-        const cachePromises = urlsToCache.map(urlToCache => {
-            return cache.add(urlToCache).catch(err => {
-                console.warn(`Failed to cache ${urlToCache}:`, err);
-            });
-        });
-        return Promise.all(cachePromises);
-      })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('pwa-cache').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './app.js',
+        './manifest.json',
+        './icons/icon-192.png',
+        './icons/icon-512.png'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
